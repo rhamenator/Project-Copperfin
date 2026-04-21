882db84c637b08a044c2952d960b8e434814db04
rhamenator <rhamenator@gmail.com>
2026-04-21 17:23:51 -0400

    Split string math expression function tests

--- tests/test_prg_engine_functions.cpp
+++ tests/test_prg_engine_functions.cpp
@@ -16,127 +16,6 @@ namespace
 
     using namespace copperfin::test_support;
 
-    void test_string_and_math_expression_functions()
-    {
-        namespace fs = std::filesystem;
-        const fs::path temp_root = fs::temp_directory_path() / "copperfin_prg_engine_str_math";
-        std::error_code ignored;
-        fs::remove_all(temp_root, ignored);
-        fs::create_directories(temp_root);
-
-        const fs::path main_path = temp_root / "str_math.prg";
-        write_text(
-            main_path,
-                "l = LEN('hello')\n"
-            "lft = LEFT('hello', 3)\n"
-            "rgt = RIGHT('hello', 2)\n"
-            "up = UPPER('hello')\n"
-            "lo = LOWER('WORLD')\n"
-            "sp = LEN(SPACE(5))\n"
-            "repl = REPLICATE('ab', 3)\n"
-            "trimmed = LTRIM('  hi  ')\n"
-            "rtrimmed = RTRIM('  hi  ')\n"
-            "a = ABS(-7)\n"
-            "b = INT(3.9)\n"
-            "c = MOD(10, 3)\n"
-            "d = ROUND(3.567, 2)\n"
-            "e = SIGN(-5)\n"
-            "f = IIF(.T., 'yes', 'no')\n"
-            "g = BETWEEN(5, 1, 10)\n"
-            "h = OCCURS('l', 'hello world')\n"
-            "v = VAL('42')\n"
-            "at_second = AT('ha', 'ha ha ha', 2)\n"
-            "rat_second = RAT('ha', 'ha ha ha', 2)\n"
-            "atc_hit = ATC('FOX', 'red fox')\n"
-            "ratc_hit = RATC('FOX', 'fox red fox')\n"
-            "line_text = 'alpha' + CHR(13) + CHR(10) + 'Beta fox' + CHR(10) + 'gamma fox'\n"
-            "atline_hit = ATLINE('fox', line_text)\n"
-            "atcline_hit = ATCLINE('BETA', line_text)\n"
-            "atline_second = ATLINE('fox', line_text, 2)\n"
-            "ratline_hit = RATLINE('fox', line_text)\n"
-            "chrtran_value = CHRTRAN('a1b2c3', '123', 'xyz')\n"
-            "chrtranc_value = CHRTRANC('aAbBcc', 'AB', 'xy')\n"
-            "chrtranc_delete = CHRTRANC('Alpha Beta', 'AE', 'x')\n"
-            "proper_value = PROPER('legacy fox-pro APP')\n"
-            "str_default = STR(42.7)\n"
-            "str_width = STR(42, 5)\n"
-            "str_decimals = STR(42.678, 8, 2)\n"
-            "str_overflow = STR(123456, 3)\n"
-            "like_hit = LIKE('A?C*', 'abc legacy')\n"
-            "like_miss = LIKE('A?D*', 'abc legacy')\n"
-            "inlist_hit = INLIST('beta', 'alpha', 'beta', 'gamma')\n"
-            "inlist_miss = INLIST(4, 1, 2, 3)\n"
-                "getwordcount_1 = GETWORDCOUNT('one two three')\n"
-                "getwordcount_2 = GETWORDCOUNT('a,b,c', ',')\n"
-                "getwordnum_1 = GETWORDNUM('one two three', 2)\n"
-                "getwordnum_2 = GETWORDNUM('a,b,c', 3, ',')\n"
-            "RETURN\n");
-
-        copperfin::runtime::PrgRuntimeSession session = copperfin::runtime::PrgRuntimeSession::create({.startup_path = main_path.string(),
-                                                                                                       .working_directory = temp_root.string(),
-                                                                                                       .stop_on_entry = false});
-
-        const auto state = session.run(copperfin::runtime::DebugResumeAction::continue_run);
-        expect(state.completed, "string/math function script should complete");
-
-        const auto check = [&](const std::string &name, const std::string &expected)
-        {
-            const auto it = state.globals.find(name);
-            if (it == state.globals.end())
-            {
-                expect(false, name + " variable not found");
-                return;
-            }
-            expect(copperfin::runtime::format_value(it->second) == expected,
-                   name + " expected '" + expected + "' got '" + copperfin::runtime::format_value(it->second) + "'");
-        };
-
-        check("l", "5");
-        check("lft", "hel");
-        check("rgt", "lo");
-        check("up", "HELLO");
-        check("lo", "world");
-        check("sp", "5");
-        check("repl", "ababab");
-        check("trimmed", "hi  ");
-        check("rtrimmed", "  hi");
-        check("a", "7");
-        check("b", "3");
-        check("c", "1");
-        check("d", "3.57");
-        check("e", "-1");
-        check("f", "yes");
-        check("g", "true");
-        check("h", "3");
-        check("v", "42");
-        check("at_second", "4");
-        check("rat_second", "4");
-        check("atc_hit", "5");
-        check("ratc_hit", "9");
-        check("atline_hit", "2");
-        check("atcline_hit", "2");
-        check("atline_second", "3");
-        check("ratline_hit", "3");
-        check("chrtran_value", "axbycz");
-        check("chrtranc_value", "xxyycc");
-        check("chrtranc_delete", "xlphx Btx");
-        check("proper_value", "Legacy Fox-Pro App");
-        check("str_default", "43");
-        check("str_width", "   42");
-        check("str_decimals", "   42.68");
-        check("str_overflow", "***");
-        check("like_hit", "true");
-        check("like_miss", "false");
-        check("inlist_hit", "true");
-        check("inlist_miss", "false");
-        check("getwordcount_1", "3");
-        check("getwordcount_2", "3");
-        check("getwordnum_1", "two");
-        check("getwordnum_2", "c");
-
-        fs::remove_all(temp_root, ignored);
-    }
-
     void test_type_and_null_expression_functions()
     {
         namespace fs = std::filesystem;
@@ -514,7 +393,6 @@ namespace
 
 int main()
 {
-    test_string_and_math_expression_functions();
     test_type_and_null_expression_functions();
     test_count_to_array_expression();
     test_sum_and_average_to_array_expression();
