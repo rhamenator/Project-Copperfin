1cabca761656425ee714953c68f9123b6aeb6e25
rhamenator <rhamenator@gmail.com>
2026-06-19 22:05:15 -0400

    Preserve macOS FLL FoxTable export checks

--- tests/test_runtime_pipeline.cpp
+++ tests/test_runtime_pipeline.cpp
@@ -491,7 +491,7 @@ std::set<std::string> read_native_exported_symbols(const std::filesystem::path& 
         if (!symbol.empty() && symbol.front() == '_') {
             symbol.erase(symbol.begin());
         }
-        if (symbol.empty() || symbol.front() == '_') {
+        if (symbol.empty() || (symbol.front() == '_' && symbol != "_FoxTable")) {
             continue;
         }
 #else
