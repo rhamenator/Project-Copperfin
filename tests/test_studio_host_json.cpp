4050f80d3f73a7468e576cd6fff19c27a575da34
rhamenator <rhamenator@gmail.com>
2026-06-28 03:06:44 -0400

    Add deleted subtree duplicate regression

--- tests/test_studio_host_json.cpp
+++ tests/test_studio_host_json.cpp
@@ -72276,6 +72276,7 @@ void test_studio_host_json_duplicates_deleted_report_visual_object_subtrees_by_s
     std::error_code ignored;
     fs::remove_all(temp_root, ignored);
     fs::create_directories(temp_root);
+    ScopedDefaultLocaleCatalogEnvironment default_locale_environment;
 
     const auto mark_middle_deleted = [](const fs::path& asset_path) {
         const auto delete_result = copperfin::vfp::set_visual_object_deleted_state({
