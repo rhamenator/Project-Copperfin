bfb41b42d0d85bc4c5afb588e942dc40472d5770
rhamenator <rhamenator@gmail.com>
2026-06-30 07:14:26 -0400

    test: retire broad report duplicate batch smoke

--- tests/test_studio_host_json.cpp
+++ tests/test_studio_host_json.cpp
@@ -321,8 +321,10 @@ int main(int argc, char** argv) {
     test_studio_host_json_renames_report_visual_object_batches_by_stable_selection(argv[1]);
     test_studio_host_json_renames_deleted_report_visual_object_batches_by_stable_selection(argv[1]);
 #endif
+#if !defined(COPPERFIN_REPORT_VISUAL_DUPLICATE_BATCH_SKIP_HOST_SMOKE)
     test_studio_host_json_duplicates_report_visual_object_batches_by_stable_selection(argv[1]);
     test_studio_host_json_duplicates_deleted_report_visual_object_batches_by_stable_selection(argv[1]);
+#endif
     test_studio_host_json_reorders_report_visual_object_batches_by_stable_selection(argv[1]);
     test_studio_host_json_reorders_deleted_report_visual_object_batches_by_stable_selection(argv[1]);
     test_studio_host_json_clears_properties_by_stable_selectors(argv[1]);
