9476173e137f4b8a0324abfa054dc189e75591ab
rhamenator <rhamenator@gmail.com>
2026-06-30 09:38:51 -0400

    test: retire broad label layout actions

---
+++ tests/test_studio_host_label_layout_actions.cpp
@@ -0,0 +1,24 @@
+#include "test_studio_host_json_support.h"
+
+using namespace cf_test_studio_host_json;
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "usage: test_studio_host_label_layout_actions <copperfin_studio_host>\n";
+        return 2;
+    }
+
+    test_studio_host_json_exposes_label_layout_parity(argv[1]);
+    test_studio_host_json_nudges_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_aligns_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_resizes_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_snaps_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_deletes_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_restores_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_distributes_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_reorders_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_duplicates_label_layout_objects_by_stable_selectors(argv[1]);
+    test_studio_host_json_renames_label_layout_object_identity_by_stable_selectors(argv[1]);
+
+    return failures == 0 ? 0 : 1;
+}
