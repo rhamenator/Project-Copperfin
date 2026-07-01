82957abfed73268f93e6c0cbfa75d27721ef75af
rhamenator <rhamenator@gmail.com>
2026-07-01 17:25:31 -0400

    Fixes #3126: cover real label column-header metadata

--- vsix/Copperfin.DesignerSmokeTests/Program.cs
+++ vsix/Copperfin.DesignerSmokeTests/Program.cs
@@ -3327,6 +3327,31 @@ internal static class Program
                 });
             SmokeAssetEditorSectionMetadataSelectionWithRealAsset(
                 TryResolveVfpSourceAsset("VFPSource/Wizards/wzreport/STYLES/STYLELBL.LBX"),
+                recordIndex: 2,
+                expectedSectionListTitle: "Column Header",
+                expectedProperties: new[]
+                {
+                    new KeyValuePair<string, string>("SECTIONSTATE", "Live"),
+                    new KeyValuePair<string, string>("BANDKIND", "Column Header"),
+                    new KeyValuePair<string, string>("RECORDINDEX", "2"),
+                    new KeyValuePair<string, string>("OBJECTCOUNT", "0"),
+                    new KeyValuePair<string, string>("DELETEDOBJECTCOUNT", "0"),
+                    new KeyValuePair<string, string>("TOP", "0"),
+                    new KeyValuePair<string, string>("HEIGHT", "0")
+                },
+                expectedMissingProperties: new[]
+                {
+                    "EXPR",
+                    "EXPRESSIONFIELD",
+                    "EXPRESSIONMEMO",
+                    "GROUPINGEXPRESSION",
+                    "GROUPINGEXPRESSIONFIELD",
+                    "GROUPINGEXPRESSIONMEMO",
+                    "GROUPPARTNERSTATE"
+                },
+                expectedObjectListCount: 0);
+            SmokeAssetEditorSectionMetadataSelectionWithRealAsset(
+                TryResolveVfpSourceAsset("VFPSource/Wizards/wzreport/STYLES/STYLELBL.LBX"),
                 recordIndex: 3,
                 expectedSectionListTitle: "Detail",
                 expectedProperties: new[]
