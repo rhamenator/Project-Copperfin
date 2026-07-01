23c8161f5e1279f1340d368e7793cd2437483c32
rhamenator <rhamenator@gmail.com>
2026-07-01 16:48:35 -0400

    Fixes #3117: cover nested real group-header metadata

--- vsix/Copperfin.DesignerSmokeTests/Program.cs
+++ vsix/Copperfin.DesignerSmokeTests/Program.cs
@@ -3164,6 +3164,32 @@ internal static class Program
                 },
                 expectedObjectListCount: 8);
             SmokeAssetEditorSectionMetadataSelectionWithRealAsset(
+                TryResolveVfpSourceAsset("VFPSource/foxref/foxrefresultsa4.frx"),
+                recordIndex: 3,
+                expectedSectionListTitle: "Group Header - RefID",
+                expectedProperties: new[]
+                {
+                    new KeyValuePair<string, string>("SECTIONSTATE", "Live"),
+                    new KeyValuePair<string, string>("BANDKIND", "Group Header"),
+                    new KeyValuePair<string, string>("RECORDINDEX", "3"),
+                    new KeyValuePair<string, string>("OBJECTCOUNT", "0"),
+                    new KeyValuePair<string, string>("DELETEDOBJECTCOUNT", "0"),
+                    new KeyValuePair<string, string>("TOP", "0"),
+                    new KeyValuePair<string, string>("HEIGHT", "2084"),
+                    new KeyValuePair<string, string>("GROUPROLE", "Header"),
+                    new KeyValuePair<string, string>("GROUPINGINDEX", "1"),
+                    new KeyValuePair<string, string>("GROUPINGNESTINGDEPTH", "1"),
+                    new KeyValuePair<string, string>("EXPR", "RefID"),
+                    new KeyValuePair<string, string>("EXPRESSIONFIELD", "6"),
+                    new KeyValuePair<string, string>("EXPRESSIONMEMO", "23"),
+                    new KeyValuePair<string, string>("GROUPINGEXPRESSION", "RefID"),
+                    new KeyValuePair<string, string>("GROUPINGEXPRESSIONFIELD", "6"),
+                    new KeyValuePair<string, string>("GROUPINGEXPRESSIONMEMO", "23"),
+                    new KeyValuePair<string, string>("GROUPPARTNERSECTIONID", "_0RV0UV0IG"),
+                    new KeyValuePair<string, string>("GROUPPARTNERRECORD", "5"),
+                    new KeyValuePair<string, string>("GROUPPARTNERSTATE", "Live")
+                });
+            SmokeAssetEditorSectionMetadataSelectionWithRealAsset(
                 TryResolveVfpSourceAsset("VFPSource/Wizards/wzapp/template/Books/Reports/by_author.FRX"),
                 recordIndex: 4,
                 expectedSectionListTitle: "Detail",
