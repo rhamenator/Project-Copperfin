e382828c644675fba2142677ca915e2df88571f6
rhamenator <rhamenator@gmail.com>
2026-07-19 06:17:31 -0400

    Trace Unicode bridge path assembly

--- tests/test_runtime_host_debug_output_formatting_bridge.cpp
+++ tests/test_runtime_host_debug_output_formatting_bridge.cpp
@@ -583,6 +583,7 @@ void test_runtime_host_decodes_unicode_bridge_descriptor_paths(const std::string
     const fs::path source_path = temp_root / "content" / source_name;
     const fs::path request_path = temp_root / "GetAnswer.request.json";
     const fs::path response_path = temp_root / "nested" / "GetAnswer.response.json";
+    std::cerr << "END: Unicode bridge path assembly\n";
     std::error_code ignored;
     fs::remove_all(temp_root, ignored);
     fs::create_directories(source_path.parent_path());
